aad010e9e8f2b4360dd7db03d1e40724e477aff2
Renato Amaral Rother <renato.rother@hotmail.com>
2026-02-25 23:34:51 -0300

    Refactor script to implement a nested loop for grid display instead of a counter

--- exercicio7.py
+++ exercicio7.py
@@ -1,14 +1,11 @@
-contador = 0
+qtd_linhas = 5
+qtd_colunas = 5
 
-while contador <= 100 :
-    print(contador)
-    contador += 1
-
-    if contador == 6:
-        print('Não vou exibir o 6')
-        continue
-
-    if contador == 40:
-        print(contador)
-        break
-
+linha = 1
+while linha <= qtd_linhas :
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+print('Acabou')
